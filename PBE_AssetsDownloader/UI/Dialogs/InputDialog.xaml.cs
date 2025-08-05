177d9b2401158bb48dcd3e4d7ef31f1e58ff43be
Neinndall <66384195+Neinndall@users.noreply.github.com>
2025-08-06 01:45:08 +0200

    better design

--- PBE_AssetsDownloader/UI/Dialogs/InputDialog.xaml.cs
+++ PBE_AssetsDownloader/UI/Dialogs/InputDialog.xaml.cs
@@ -1,4 +1,5 @@
 using System.Windows;
+using System.Windows.Input;
 
 namespace PBE_AssetsDownloader.UI.Dialogs
 {
@@ -27,6 +28,12 @@ namespace PBE_AssetsDownloader.UI.Dialogs
         private void btnCancel_Click(object sender, RoutedEventArgs e)
         {
             DialogResult = false;
+        }
+
+        private void TitleBar_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                this.DragMove();
         }
     }
 }
